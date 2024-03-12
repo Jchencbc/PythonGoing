a2c27ab728d57dc108f7183662ff13c41fabbb09
jinchen <278229887@qq.com>
2024-03-12 16:28:22 +0800

    python 实现链表

---
+++ DataStuctur/ChainStuctur.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Author:jinchen
+Email:278229887@qq.com
+date:12/3/2024 下午 2:07
+"""
+
+"""
+链表
+"""
+class Node:
+    def __init__(self, value=None, next=None):
+        self._value = value  # 数据
+        self._next = next  # 指向下个节点
+
+    def getValue(self):
+        return self._value
+
+    def getNext(self):
+        return self._next
+
+    def setValue(self, new_value):
+        self._value = new_value
+
+    def setNext(self, new_next):
+        self._next = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self._head = Node()
+        self._tail = None
+        # self._length = 0
+
+    def isEmpty(self):
+        return self._head is None
+
+    def add(self, value):
+        """链表前端添加元素"""
+        node = Node()
+        node.setValue(value)
+        node.setNext(self._head)
+        self._head = node
+        # self._length += 1
+
+    def append(self, value):
+        """尾部添加元素"""
+        node = Node()
+        node.setValue(value)
+        if self.isEmpty():
+            self._head = node  # 若为空表，将添加的元素设为第一个元素
+        else:
+            current = self._head
+            while current.getNext() is not None:
+                current = current.getNext()  # 遍历链表
+            current.setNext(node)  # 此时current为链表最后的元素
+
+    def search(self, value):
+        """查找元素"""
+        ...
+
+    def index(self, value):
+        """查找元素返回下标"""
+        ...
+
+    def remove(self, value, index):
+        """删除元素"""
+        ...
+
+    def insert(self, value, index):
+        """插入元素"""
+        ...
+
+
+if __name__ == "__main__":
+    ...
